f6f1dd6e50c11b6f9592fb8c4f4cb48244d40b0b
TollerNamen <david.grimme@gmx.de>
2023-04-06 18:07:44 +0200

    Set 'addCommandsToGuild' to default to 'false'

--- src/main/kotlin/Config.kt
+++ src/main/kotlin/Config.kt
@@ -2,6 +2,6 @@ import java.awt.Color
 import java.io.File
 
 val tokenFile = File("/home/admindavid/IdeaProjects/Token/FidelitasToken.txt")
-val guildIds: List<String> = listOf("832328391347666964", "979105761084968960")
-val addCommandsToGuild = true
+val guildIds: List<String> = listOf("832328391347666964")
+val addCommandsToGuild = false
 val embedColor = Color(0x040791)
