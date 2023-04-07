5b53b50758a8a1fb25f5cbd4257732e751aed0f4
TollerNamen <david.grimme@gmx.de>
2023-04-07 21:56:52 +0200

    Added Python and Javascript

--- src/main/kotlin/CommandHandler.kt
+++ src/main/kotlin/CommandHandler.kt
@@ -1,12 +1,13 @@
 import commands.about
 import commands.help
+import commands.pythonTest
+import commands.stats.memberstats
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.build.CommandData
 import net.dv8tion.jda.api.interactions.commands.build.Commands
 import net.dv8tion.jda.api.interactions.commands.build.OptionData
-import commands.stats.memberstats
 
 val cmds: List<CommandData> = listOf(
     Commands.slash("statistics", "show statistics")
@@ -18,15 +19,17 @@ val cmds: List<CommandData> = listOf(
                 .addChoice("Server", "server")
                 .addChoice("User", "user")),
     Commands.slash("help", "show all available commands"),
-    Commands.slash("about", "show what the application is all about")
+    Commands.slash("about", "show what the application is all about"),
+    Commands.slash("pythontest", "test python implementation")
 )
 class CommandHandler(private val event: SlashCommandInteractionEvent) : ListenerAdapter()
 {
     private val commandName: String = event.name
     private val subCommandName: String = if (event.options.size > 0) {
         event.options[0].name
-    } else {
-        // Default subcommand name if no options are present
+    }
+    else
+    {
         "defaultSubCommand"
     }
     fun commandHandler()
@@ -36,6 +39,7 @@ class CommandHandler(private val event: SlashCommandInteractionEvent) : Listener
             "about" -> {about(event)}
             "help" -> {help(event)}
             "statistics" -> {statisticSubCommand()}
+            "pythontest" -> {pythonTest(event)}
             else -> {errorMessage("Error: This Command does not exist in this FidelitasClient")}
         }
     }
